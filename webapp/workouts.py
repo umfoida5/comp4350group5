2664eb3a04b8aa9caf25edd8b1d00e1c46e910ff
EC2 Default User <ec2-user@ip-10-244-142-140.ec2.internal>
2013-02-03 18:53:59 +0000

    fixed error

--- webapp/workouts.py
+++ webapp/workouts.py
@@ -1,11 +1,7 @@
 import cherrypy
-from modules import datatables
 
 class Workouts:
-    @cherrypy.tools.json_out() 
     @cherrypy.expose
     def index(self, **params):
-        table = 'workouts'
-        columns = ['name', 'distance']
-        return datatables.getresponse(table, columns, params)
+        return "test"
     
