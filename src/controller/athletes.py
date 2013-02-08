a365b8d8c9032a2e05ec0a684f7dda5a4de1bf95
Blake Beatty <umbeatty@cc.umanitoba.ca>
2013-02-08 13:17:48 -0600

    Temporary controller changes to display view/edit/new pages for athletes and activities

--- src/controller/athletes.py
+++ src/controller/athletes.py
@@ -9,6 +9,21 @@ class Athletes:
         tmpl = env.get_template('athletes.html')
         return tmpl.render()
 
+    @cherrypy.expose
+    def new(self):
+        tmpl = env.get_template('athletes_new.html')
+        return tmpl.render()
+
+    @cherrypy.expose
+    def edit(self):
+        tmpl = env.get_template('athletes_edit.html')
+        return tmpl.render()
+
+    @cherrypy.expose
+    def view(self):
+        tmpl = env.get_template('athlete_view.html')
+        return tmpl.render()
+
     @cherrypy.tools.json_out()
     @cherrypy.expose
     def json(self, **params):
