b24fa90082867b0c7438e0b96e58be54f86615c6
PLatka <umlatkap@cc.umanitoba.ca>
2013-02-09 09:40:03 -0600

    Added some skeletal test code

--- test/controller/root_test.py
+++ test/controller/root_test.py
@@ -3,13 +3,15 @@ import cherrypy
 import sys
 import os
 from jinja2 import Environment, FileSystemLoader
-sys.path.append('../../controller')
-from root import Root
+sys.path.append('../../src')
+from controller.root import Root
 
 env = Environment(loader=FileSystemLoader('view/web/templates'))
 
-class RootTest:
+class RootTest(unittest.TestCase):
 	
 	def indexTest(self):
 		return True
 		
+if(__name__ == '__main__'):
+	unittest.main()
