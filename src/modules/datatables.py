61904233b8bb59ec3031111a375484254faab24b
Alex Salomon <alexsalomon.th@gmail.com>
2013-02-16 16:50:55 -0600

    Return the sorting datatable functionality

--- src/modules/datatables.py
+++ src/modules/datatables.py
@@ -7,6 +7,15 @@ def send_datatable_response(query, search_filter, request_params):
     if search_filter is not None:
         filtered_query = query.filter(search_filter)
 
+    sort_col = request_params.get(
+        "mDataProp_%s" % int(request_params.get("iSortCol_0", 0))
+    )
+    if sort_col:
+        sort_direction = request_params.get("sSortDir_0", "asc")
+        sorted_query = filtered_query.order_by(
+            "%s %s" % (sort_col, sort_direction)
+        )
+
     rows = filtered_query.limit(
         request_params.get('iDisplayLength', 10)
     ).offset(
