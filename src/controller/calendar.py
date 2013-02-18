2545f19cc1d82e2bdd10dc6eddd2b3a1507093cf
Blake Beatty <umbeatty@cc.umanitoba.ca>
2013-02-17 20:52:26 -0600

    Calendar populate events from database

--- src/controller/calendar.py
+++ src/controller/calendar.py
@@ -1,8 +1,17 @@
 import cherrypy
 from modules.template import env
-
+from model.activity import Activity
+from modules.jsonable import make_jsonable
 class Calendar:
     @cherrypy.expose
     def index(self):
         tmpl = env.get_template('calendar_view.html')
         return tmpl.render()
+
+    @cherrypy.tools.json_out()
+    @cherrypy.expose
+    @cherrypy.tools.allow(methods=['GET'])
+    def get(self):
+        activities = Activity.query.all()
+        json_data = {"activities":make_jsonable(activities)}      
+        return json_data
