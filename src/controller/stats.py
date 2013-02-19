1c4fd632e4963e9c87f294ceba7d836a6b81420b
Andrew <cr-h72s3@hotmail.com>
2013-02-19 14:18:10 -0600

    added code to request JSON objects

--- src/controller/stats.py
+++ src/controller/stats.py
@@ -1,5 +1,8 @@
 import cherrypy
+import sys
+import statistics_engine
 from modules.template import env
+from modules.jsonable import make_jsonable
 
 class Stats:
     @cherrypy.expose
@@ -11,3 +14,9 @@ class Stats:
     def create(self):
         tmpl = env.get_template('stats.html')
         return tmpl.render()
+
+    @cherrypy.tools.json_out()
+    @cherrypy.expose
+    def get_total(self, **params):
+    	result = StatisticsEngine.total.get(1)
+	return make_jsonable(result)
