e4a487dfb050715f52dc0f3b32418e3bca7d64ce
Alex Salomon <alexsalomon.th@gmail.com>
2013-02-20 01:04:41 -0600

    Add unlocked_date to ath/ach association table
    To make this change possible, the type of relationship between athletes and achievements had to change, as well as other smaller details in order to get badges working

--- src/controller/achievements.py
+++ src/controller/achievements.py
@@ -1,9 +1,8 @@
 import cherrypy, httplib
-from modules.transaction import commit_on_success
 from modules.jsonable import make_jsonable
 from modules.template import env
 from model.athlete import Athlete
-from model.achievement import Achievement, AthleteAchievements
+from model.achievement import Achievement, UnlockedAchievement
 
 class Achievements:
 
@@ -15,19 +14,8 @@ class Achievements:
     @cherrypy.tools.json_out()
     @cherrypy.expose
     @cherrypy.tools.allow(methods=['GET'])
-    def get_athlete_achievements(self):
-        athlete_achievements = (Athlete.query.get(1)).achievements
-        return make_jsonable(athlete_achievements)
-
-    @cherrypy.tools.json_out()
-    @cherrypy.expose
-    @commit_on_success
-    @cherrypy.tools.allow(methods=['POST'])
-    def unlock(self, athlete_id, achievement_id):
-        athlete = Athlete.query.get(athlete_id)
-        achievement = Achievement.query.get(achievement_id)
-        if athlete != None and achievement != None:
-            athlete.achievements.append(achievement)
-            cherrypy.response.status = httplib.NO_CONTENT
-        else:
-            raise cherrypy.HTTPError(httplib.NOT_FOUND)
+    def get_unlocked_achievements(self):
+        unlocked_achievements = UnlockedAchievement.query.filter_by(
+            athlete_id=1
+        ).all()
+        return make_jsonable(unlocked_achievements)
