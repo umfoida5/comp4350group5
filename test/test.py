ac84aeedd28b9cebb8f9b21695ac341e9bca231e
umfoida5 <umfoida5@cc.umanitoba.ca>
2013-02-20 15:17:50 -0600

    Added event model tests.

--- test/test.py
+++ test/test.py
@@ -11,6 +11,7 @@ from controller_tests.achievements_test import AchievementsTest
 from controller_tests.statistics_engine_test import StatisticsEngineTest
 from model_tests.achievement_test import AchievementTests
 from model_tests.athlete_test import AthleteTests
+from model_tests.event_test import EventTests
 from modules_tests.jsonable_test import JsonableTests
 from modules_tests.transaction_test import TransactionTests
 
