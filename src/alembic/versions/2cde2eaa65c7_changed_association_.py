275a169011c5b0cd9c3cc5fb01a9cdcb8b4349ac
Alex Salomon <alexsalomon.th@gmail.com>
2013-02-21 16:45:49 -0600

    update migration

--- src/alembic/versions/2cde2eaa65c7_changed_association_.py
+++ src/alembic/versions/2cde2eaa65c7_changed_association_.py
@@ -29,10 +29,19 @@ def upgrade():
     )
     op.drop_table(u'athlete_achievements')
     op.drop_column('achievements', u'creation_date')
+    op.execute("DELETE FROM achievements")
     op.create_unique_constraint("uq_achievements_title", "achievements", ["title"])
     op.alter_column('achievements', 'image_url',
                existing_type=sa.VARCHAR(length=256),
                nullable=False)
+
+    op.execute("INSERT INTO achievements (title, description, image_url) VALUES ('Runner', 'Yay! You have achieved the title of runner', '../img/achievements/unlocked_achievement4.jpeg')")
+    op.execute("INSERT INTO achievements (title, description, image_url) VALUES ('Crazy', 'Yaaaahaaa, WOwhooo, beepee beepee', '../img/achievements/unlocked_achievement3.jpeg')")
+    op.execute("INSERT INTO achievements (title, description, image_url) VALUES ('Local', 'You must know all the locals by now - you are always running around!', '../img/achievements/unlocked_achievement5.jpeg')")
+    op.execute("INSERT INTO achievements (title, description, image_url) VALUES ('Adventurer', 'Walk through the same route everyday? Thats not for you - congratulations, you have acquired the adventurer badge', '../img/achievements/unlocked_achievement6.jpeg')")
+    op.execute("INSERT INTO achievements (title, description, image_url) VALUES ('Inspired', 'You have been biking everyday since 1993! That deserves a celebrtion!','../img/achievements/unlocked_achievement7.jpeg')")
+    op.execute("INSERT INTO achievements (title, description, image_url) VALUES ('Forest Gump', 'Run Forest, RUN!', '../img/achievements/unlocked_achievement8.jpeg')")
+
 
     ### end Alembic commands ###
 
