39e0d033b92c816738767feaf4cbdff6782177fa
Andrew <cr-h72s3@hotmail.com>
2013-02-21 04:32:17 -0600

    stats successfully calls JSON
    results are still empty at this moment. I suspect this is because I am
    not getting the correct athlete_id (or user id)

--- src/controller/stats.py
+++ src/controller/stats.py
@@ -1,6 +1,7 @@
 import cherrypy
 import sys
-import controller.statistics_engine
+from controller.statistics_engine import StatisticsEngine
+from modules.datatables import send_datatable_response
 from modules import database
 from modules.template import env
 from modules.jsonable import make_jsonable
@@ -12,13 +13,11 @@ class Stats:
         tmpl = env.get_template('stats.html')
         return tmpl.render()
 
-    @cherrypy.expose
-    def create(self):
-        tmpl = env.get_template('stats.html')
-        return tmpl.render()
-
     @cherrypy.tools.json_out()
     @cherrypy.expose
     def get_total(self, **params):
-    	result = StatisticsEngine.total(StatisticsEngine, params)
+    	mySE = StatisticsEngine()
+    	result = StatisticsEngine.total(mySE, **params)
+    	print "the results of the call: ",
+    	print result
 	return make_jsonable(result)
