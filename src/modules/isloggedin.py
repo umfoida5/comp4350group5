93749c95fa8d53927723a9f5558a5ce8dd2ca78d
umfoida5 <umfoida5@cc.umanitoba.ca>
2013-03-06 20:43:01 -0600

    Fixed Athlete user default info to make it clear it is editable.

--- src/modules/isloggedin.py
+++ src/modules/isloggedin.py
@@ -6,12 +6,13 @@ from model.athlete import Athlete
 #Creates a temporary userID in the database
 #so that users can use the site without
 #having to login.
+@commit_on_success
 def isloggedin():
 	if cherrypy.session.get('id') is None:
 		db_session = database.session
-		athlete = Athlete(None, None, "FirstName", "LastName")
+		athlete = Athlete(None, None, "FirstName", "LastName", "Click to Edit Email", "1980-03-25", "Click to Edit", "Click to Edit Address")
 		db_session.add(athlete)
-		db_session.commit()
+		db_session.flush()
 		#TODO, need a way to get the athlete that we just created
 		#to set the session ID. Don't think this works.
 		cherrypy.session['id'] = athlete.id
