9c5ad12cf38e71f5fc3bb4d727de1105f4449186
Justin Foidart <umfoida5@nutria.cs.umanitoba.ca>
2013-03-08 16:19:41 -0600

    Added About IOS page.

---
+++ src/view/iOS/CardioQuest/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  CardioQuest
+//
+//  Created by Justin Foidart on 2013-03-08.
+//  Copyright (c) 2013 comp4350group5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
