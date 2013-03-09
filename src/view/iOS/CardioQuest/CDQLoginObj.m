b36f740d980c22a7d3aa4d8de152f8f9e5678f3d
Blake Andrew Beatty <umbeatty@mouse.cs.umanitoba.ca>
2013-03-08 23:09:34 -0600

    restkit framework integration

---
+++ src/view/iOS/CardioQuest/CDQLoginObj.m
@@ -0,0 +1,13 @@
+//
+//  CDQLoginObj.m
+//  CardioQuest
+//
+//  Created by Blake Andrew Beatty on 2013-03-08.
+//  Copyright (c) 2013 comp4350group5. All rights reserved.
+//
+
+#import "CDQLoginObj.h"
+
+@implementation CDQLoginObj
+
+@end
