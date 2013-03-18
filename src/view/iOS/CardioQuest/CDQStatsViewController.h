d1a91e3a07395b799c55e27a958d976db091c9c9
Andrew <crh72s3@gmail.com>
2013-03-18 16:57:15 -0500

    Revert "Revert "adding stats view (for the third time)""
    This reverts commit ae8cb7942f14ea753ee52774f4200514f7fcbe29.
    
    Conflicts:
    	src/view/iOS/CardioQuest.xcodeproj/project.pbxproj
    	src/view/iOS/CardioQuest/en.lproj/MainStoryboard_iPad.storyboard

---
+++ src/view/iOS/CardioQuest/CDQStatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDQStatsViewController.h
+//  CardioQuest
+//
+//  Created by Andrew Konkin on 3/16/13.
+//  Copyright (c) 2013 comp4350group5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDQStatsViewController : UIViewController
+
+@end
