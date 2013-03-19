f2cbe03bd1ab1ad0aad6aae7a11c88cabe69d9c9
Andrew <crh72s3@gmail.com>
2013-03-19 15:59:15 -0500

    switched from core plot to ECGraph
    removed old files, added new ones. Updated build settings because
    ECGraph does it's own memory management (modern files use ARC-automatic
    reference counting)

---
+++ src/view/iOS/CardioQuest/Classes/DisplayView.h
@@ -0,0 +1,16 @@
+//
+//  DisplayView.h
+//  ECGraphic
+//
+//  Created by ris on 10-4-17.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ECGraph.h"
+
+@interface DisplayView : UIView <ECGraphDelegate>{
+
+}
+
+@end
