3522fa1df0a1796384f3930e65fb31b227abe208
Blake Beatty <beatty.blake@gmail.com>
2013-03-21 21:33:10 -0500

    Added frank tests for signup, entering activities, entering goals

---
+++ src/view/iOS/Frank/features/frank_test_events.feature
@@ -0,0 +1,29 @@
+Feature: 
+  COMP 4350 GROUP 5
+  Frank iOS UI Testing!
+  Test goals functionality
+
+##################################################
+# Tests signup functionality with an existing id
+##################################################
+Scenario: 
+    Tests Login/Logout Functionality with an invalid user
+Given I launch the app using iOS 6.1 and the ipad simulator
+
+When I navigate to "Home"
+Then I should see the login button
+
+When I touch the button marked "Login"
+
+When I log in
+Then I should be on the Home screen
+Then I should see the logout button
+
+When I navigate to "Goals"
+Then I should be on the Goals screen
+Then I should see a valid goal
+
+When I touch the button marked "Add a Goal"
+When I enter a new goal
+Then I should be on the Goals screen
+Then I should see the new goal
--- src/view/iOS/Frank/features/step_definitions/launch_steps.rb
+++ src/view/iOS/Frank/features/step_definitions/launch_steps.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 def app_path
   ENV['APP_BUNDLE_PATH'] || (defined?(APP_BUNDLE_PATH) && APP_BUNDLE_PATH)
 end
@@ -34,20 +36,193 @@ When(/^I navigate to "(.*?)"$/) do |tab_button|
     sleep 2
 end
 
+When(/^I touch the navbar item "(.*?)"$/) do |tab_button|
+    check_element_exists "view:'UITabBarButton' marked:'#{tab_button}'"
+    touch "view:'UINavigationItemButtonView' marked:'#{tab_button}'"
+    sleep 2
+end
 
-When(/^I enter login credentials$/) do
+###
+#
+# Enters login credentials for the iOS Page
+###
+When(/^I log in$/) do
     text_field_selector = "view:'UITextField' marked:'userNameLogin'"
     check_element_exists(text_field_selector);
     touch text_field_selector
-    frankly_map text_field_selector, "setText:", "justin"
+    frankly_map text_field_selector, "setText:", "frank"
     frankly_map text_field_selector, "endEditing:", true
     
     text_field_selector = "view:'UITextField' marked:'passwordLogin'"
     check_element_exists(text_field_selector);
     touch text_field_selector
-    frankly_map text_field_selector, "setText:", "justin"
+    frankly_map text_field_selector, "setText:", "frank"
     frankly_map text_field_selector, "endEditing:", true
+    sleep 2
+    touch "view:'UIRoundedRectButton' marked:'Login'"
+    sleep 2
 end
+
+###
+#
+# Enters invalid login credentials
+###
+When(/^I enter an invalid log in password$/) do
+    text_field_selector = "view:'UITextField' marked:'userNameLogin'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "frank"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'passwordLogin'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "incorrect_password"
+    frankly_map text_field_selector, "endEditing:", true
+    sleep 2
+    touch "view:'UIRoundedRectButton' marked:'Login'"
+    sleep 2
+end
+
+###
+#
+# Enters invalid login credentials
+###
+When(/^I enter an invalid log in$/) do
+    text_field_selector = "view:'UITextField' marked:'userNameLogin'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "this_is_an_invalid_login"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'passwordLogin'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "some_password"
+    frankly_map text_field_selector, "endEditing:", true
+    sleep 2
+    touch "view:'UIRoundedRectButton' marked:'Login'"
+    sleep 2
+end
+
+###
+#
+# Signs up with an existing id
+###
+When(/^I signup with an existing id$/) do
+    text_field_selector = "view:'UITextField' marked:'signupUsername'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "frank"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'signupPassword'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "frank"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'signupFirstName'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "frank"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'signupLastName'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "frank"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    sleep 2
+    touch "view:'UIRoundedRectButton' marked:'Sign up'"
+    sleep 2
+end
+
+###
+#
+# Signs up with a valid id
+###
+When(/^I signup with a new id$/) do
+    
+    current_time = Time.now;
+    new_user = "newuser#{current_time.year}#{current_time.month}#{current_time.day}#{current_time.hour}#{current_time.min}#{current_time.sec}"
+    
+    text_field_selector = "view:'UITextField' marked:'signupUsername'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", new_user
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'signupPassword'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "frank"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'signupFirstName'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "frank"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'signupLastName'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "frank"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    sleep 2
+    touch "view:'UIRoundedRectButton' marked:'Sign up'"
+    sleep 2
+end
+
+###
+#
+# Signs up with a valid id
+###
+When(/^I enter a new activity$/) do
+        
+    text_field_selector = "view:'UITextField' marked:'newActivityDuration'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "12"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'newActivityMaxSpeed'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "12"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    text_field_selector = "view:'UITextField' marked:'newActivityDistance'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "12"
+    frankly_map text_field_selector, "endEditing:", true
+    
+    sleep 2
+    touch "view:'UINavigationButton' marked:'Add'"
+    sleep 2
+end
+
+###
+#
+# Signs up with a valid id
+###
+When(/^I enter a new goal$/) do
+    
+    text_field_selector = "view:'UITextField' marked:'newGoalQuantity'"
+    check_element_exists(text_field_selector);
+    touch text_field_selector
+    frankly_map text_field_selector, "setText:", "12"
+    frankly_map text_field_selector, "endEditing:", true
+        
+    sleep 2
+    touch "view:'UINavigationButton' marked:'Add'"
+    sleep 2
+end
+
 
 ###
 #
@@ -55,7 +230,6 @@ end
 #
 ###
 Then (/^I should be on the Home screen$/) do
-    check_element_exists "view:'UITextField' marked:'passwordLogin'"
     check_element_exists "view:'UINavigationItemView' marked:'Home'"
 end
 
@@ -82,14 +256,21 @@ end
 ###
 #
 #
-#
 ###
 Then (/^I should see a valid goal$/) do
-    check_element_exists "view:'UILabel' marked:'COMPLETED! Walk 100 (Between: 04/11/2013 21:54:55 and 05/06/2013 21:54:55)'"
+    check_element_exists "view:'UILabel' marked:'COMPLETED! Run 12 (Between: 02/26/2013 00:00:00 and 02/27/2013 00:00:00)'"
+end
+
+Then (/^I should see the new goal$/) do
+    check_element_exists "view:'UILabel' marked:'COMPLETED! Run 12 (Between: #{Time.now.strftime('%m/%d/%Y')} 00:00:00 and #{Time.now.strftime('%m/%d/%Y')} 00:00:00)"
 end
 
 Then (/^I should see a valid activity$/) do
-    check_element_exists "view:'UILabel' marked:'(2013-03-05) Bike: 35 km in 60 mins (Max Speed: 55 km/h)'"
+    check_element_exists "view:'UILabel' marked:'(2013-03-05) Run: 12 km in 12 mins (Max Speed: 12 km/h)'"
+end
+
+Then (/^I should see the new activity$/) do
+    check_element_exists "view:'UILabel' marked:'(#{Time.now.strftime('%Y-%m-%d')}) Run: 12 km in 12 mins (Max Speed: 12 km/h)'"
 end
 
 Then (/^I should see a valid event$/) do
@@ -102,4 +283,18 @@ end
 
 Then (/^I should see a valid about$/) do
     check_element_exists "view:'UILabel' marked:'CardioQuest'"
+end
+
+###
+#
+# verrifies that the logout and login buttons are shown
+###
+Then (/^I should see the login button$/) do
+    sleep 2
+    check_element_exists "view:'UINavigationButton' marked:'Login'"
+end
+
+Then (/^I should see the logout button$/) do
+    sleep 2
+    check_element_exists "view:'UINavigationButton' marked:'Logout'"
 end
