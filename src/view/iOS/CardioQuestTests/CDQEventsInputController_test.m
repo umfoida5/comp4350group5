1d53684101cff22ddcfc1f20063470cb629bca50
PLatka <umlatkap@cc.umanitoba.ca>
2013-03-24 13:51:59 -0500

    Added test for EventInputController

--- src/view/iOS/CardioQuestTests/CDQEventsInputController_test.m
+++ src/view/iOS/CardioQuestTests/CDQEventsInputController_test.m
@@ -10,4 +10,63 @@
 
 @implementation CDQEventsInputController_test
 
+// setUp()
+//
+// performs initial setup for tests, sets storyboard to the ipad storyboard
+// and sets the goalsController to the Goals
+//
+- (void)setUp
+{
+    [super setUp];
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard_iPad" bundle:nil];
+    
+    self.eiController = [storyboard instantiateViewControllerWithIdentifier:@"EventInputController"];
+    [self.eiController performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
+    
+    self.eventController = [storyboard instantiateViewControllerWithIdentifier:@"Events"];
+    [self.eventController performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
+}
+
+//
+// tearDown()
+//
+// performs tear down functionality
+//
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+//
+// waitForCompletion()
+//
+// checks checks goals table for data
+//
+- (BOOL)waitForCompletion:(NSTimeInterval)timeoutSecs {
+    NSDate *timeoutDate = [NSDate dateWithTimeIntervalSinceNow:timeoutSecs];
+    while ([[self.eventController eventsTable]numberOfRowsInSection:0] <= 1) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:timeoutDate];
+        if([timeoutDate timeIntervalSinceNow] < 0.0)
+            return NO;
+    }
+    
+    
+    
+    return YES;
+}
+
+// -- ASYNCHRONOUS TEST --
+//
+// testEventsBeforeLogin()
+//
+// Tests to see that without a login the asynchronous call returns valid objects
+//
+- (void)testCreatingANewHealth
+{
+    // Tests goals page to see that no records are populated when no user is logged in
+    [self.eiController postEvent:@"eventInputController"];
+    STAssertFalse([self waitForCompletion:3.0], @"Health Record Creation Timed out");
+}
+
 @end
